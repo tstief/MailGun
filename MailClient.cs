b84883f73320d5de98b6d435ae7cc97ee692f978
tstief <tstief@gmail.com>
2019-01-02 20:21:07 +0000

    New formatting

--- MailClient.cs
+++ MailClient.cs
@@ -8,31 +8,31 @@ using System.Collections.Generic;
 
 namespace MailGun
 {
-    public class MailClient
+  public class MailClient
+  {
+    private HttpClient Client { get; set; }
+    private string Domain { get; set; }
+    private string MailKey { get; set; }
+
+    public MailClient(string domain, string key)
     {
-        private HttpClient Client{get;set;}        
-        private string Domain {get;set;}
-        private string MailKey {get;set;}
-
-        public MailClient(string domain, string key)
-        {
-            Domain = domain;
-            MailKey = key;
-            Client = new HttpClient();
-            Client.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("Basic",
-                                              Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes("api" +
-                                               ":" + MailKey)));
-        }
-
-        public async Task<HttpResponseMessage> SendMessageAsync(string toAddress, string subject, string text)
-        {
-            var form = new Dictionary<string, string>();
-            form["from"] = "noreply@mg.stieffamily.com";
-            form["to"] = toAddress;
-            form["subject"] = subject;
-            form["text"] = text;
-            return await Client.PostAsync("https://api.mailgun.net/v3/" + Domain + "/messages", new FormUrlEncodedContent(form));
-        }
+      Domain = domain;
+      MailKey = key;
+      Client = new HttpClient();
+      Client.DefaultRequestHeaders.Authorization =
+          new AuthenticationHeaderValue("Basic",
+                                        Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes("api" +
+                                         ":" + MailKey)));
     }
+
+    public async Task<HttpResponseMessage> SendMessageAsync(string toAddress, string subject, string text)
+    {
+      var form = new Dictionary<string, string>();
+      form["from"] = "noreply@mg.stieffamily.com";
+      form["to"] = toAddress;
+      form["subject"] = subject;
+      form["text"] = text;
+      return await Client.PostAsync("https://api.mailgun.net/v3/" + Domain + "/messages", new FormUrlEncodedContent(form));
+    }
+  }
 }
